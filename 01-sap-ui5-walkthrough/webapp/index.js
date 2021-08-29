969c47bef5613820b7e69e7bee32e4580679e314
leo <40744320+leogithubid@users.noreply.github.com>
2021-08-29 12:50:52 +1000

    step4-XML View
    step4-XML View

--- 01-sap-ui5-walkthrough/webapp/index.js
+++ 01-sap-ui5-walkthrough/webapp/index.js
@@ -1,14 +1,22 @@
 sap.ui.define([
-    "sap/m/Text"
-], function(Text) {
+    //"sap/m/Text" //step4 - this is commented and replaced with XML View
+    "sap/ui/core/mvc/XMLView"
+], function(XMLView) {
     'use strict';
    
     //step 2- on init this file will be loaded and it will call an alert
     //step 3 - commented this and added text control
     //and then place it in the content 
     //alert("Get,Set,Go!");
-    new Text({
-        text : "Hello UI5!"
-    }).placeAt("content");
+    //new Text({
+    //    text : "Hello UI5!"
+    //}).placeAt("content");
+    //step 4 - the above is commented adn replaced with XML View
+    //use create method of XML View and then make use of Javascript promise
+    XMLView.create({
+        viewName : "sap.ui.demo.walkthrough.view.App"
+    }).then(function(oView){
+        oView.placeAt("content");
+    });
 
 });

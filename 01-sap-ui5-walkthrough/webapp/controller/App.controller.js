8fbe207cb9ebf867ed37ba0a2e6119d8cabfda2c
leo <40744320+leogithubid@users.noreply.github.com>
2021-08-29 13:17:02 +1000

    step6-modules
    step6-modules

--- 01-sap-ui5-walkthrough/webapp/controller/App.controller.js
+++ 01-sap-ui5-walkthrough/webapp/controller/App.controller.js
@@ -1,13 +1,19 @@
-/* Step 5 - create controller with a scaffolding template */
+/* Step 5 - create controller with a scaffolding template 
+   Step 6 - Modules - resources are called modules in UI5. So we use a Message Toast
+                      module and then raise a toast replacing the alert
+*/
 sap.ui.define([
-"sap/ui/core/mvc/Controller"
-], function(Controller) {
+"sap/ui/core/mvc/Controller",
+"sap/m/MessageToast"    //step 6 - load module and use it in the constructor argument
+], function(Controller,MessageToast) {
     'use strict';
     //Step 5 - controller.extend and give the path to the controller
     Controller.extend("sap.ui.demo.walkthrough.controller.App",{
     //Step 5 - then create the press event handler function to say hello
     onShowHello:function(){
-        alert("Hello I am controller, how are you? ");
+        //alert("Hello I am controller, how are you? ");
+        //Step 6 - the above alert is replaced with a message toast
+        MessageToast.show("Let's raise a toast for UI5");
     }
     });
 });

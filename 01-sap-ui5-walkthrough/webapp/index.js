2c9bdd3c945fcad052bf2d4b166b2981c9a0c112
leo <40744320+leogithubid@users.noreply.github.com>
2021-08-29 12:36:23 +1000

    step3-controls
    step3-controls

--- 01-sap-ui5-walkthrough/webapp/index.js
+++ 01-sap-ui5-walkthrough/webapp/index.js
@@ -1,6 +1,14 @@
 sap.ui.define([
-
-], function() {
+    "sap/m/Text"
+], function(Text) {
     'use strict';
-    alert("Get,Set,Go!");
+   
+    //step 2- on init this file will be loaded and it will call an alert
+    //step 3 - commented this and added text control
+    //and then place it in the content 
+    //alert("Get,Set,Go!");
+    new Text({
+        text : "Hello UI5!"
+    }).placeAt("content");
+
 });

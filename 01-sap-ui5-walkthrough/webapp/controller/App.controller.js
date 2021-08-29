c9c1cee5671e3e5dd6e3e6bbed6c2d7e2afcac08
leo <40744320+leogithubid@users.noreply.github.com>
2021-08-29 14:06:06 +1000

    step8-translatable texts
    step8-translatable texts

--- 01-sap-ui5-walkthrough/webapp/controller/App.controller.js
+++ 01-sap-ui5-walkthrough/webapp/controller/App.controller.js
@@ -6,8 +6,9 @@
 sap.ui.define([
 "sap/ui/core/mvc/Controller",
 "sap/m/MessageToast",    //step 6 - load module and use it in the constructor argument
-"sap/ui/model/json/JSONModel"
-], function(Controller,MessageToast,JSONModel) {
+"sap/ui/model/json/JSONModel",
+"sap/ui/model/resource/ResourceModel" //load Resource Model
+], function(Controller,MessageToast,JSONModel,ResourceModel) {
     'use strict';
     //Step 5 - controller.extend and give the path to the controller
     Controller.extend("sap.ui.demo.walkthrough.controller.App",{
@@ -21,12 +22,26 @@ sap.ui.define([
          };
          var oModel = new JSONModel(oData);
          this.getView().setModel(oModel); 
+
+         //step 8 - loading Resource model
+         var i18nModel = new ResourceModel({
+             bundleName : "sap.ui.demo.walkthrough.i18n.i18n" //give path to i18n file
+         });
+         this.getView().setModel(i18nModel,"i18n");//give it a name - i18n so that we can call it later with this name
       },
     //Step 5 - then create the press event handler function to say hello
     onShowHello:function(){
         //alert("Hello I am controller, how are you? ");
+
+        //Step 8 - get the i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        //get JSON Model data
+        var sName = this.getView().getModel().getProperty("/recipient/name");
+        //concatenate and create message
+        var oMessage = oBundle.getText("helloMsg",sName);
+
         //Step 6 - the above alert is replaced with a message toast
-        MessageToast.show("Let's raise a toast for UI5");
+        MessageToast.show(oMessage);
     }
     });
 });

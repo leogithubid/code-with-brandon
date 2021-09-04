e0acc1965cecaa2548c3987ee6d40b0880bd81eb
leo <40744320+leogithubid@users.noreply.github.com>
2021-09-04 11:57:56 +1000

    29-opa-test
    29-opa-test

---
+++ 01-sap-ui5-walkthrough/29-opa-integration-test/webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+/* Just the standard scaffolding template */
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",//load controller 
+], function (Controller, MessageToast) {
+    'use strict';
+    //controller.extend and give the path to the controller
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onInit: function () {
+            //moved to Component JS
+        },
+        onOpenDialog:function(){
+            this.getOwnerComponent().openHelloDialog();
+        }
+    });
+});

a2b01ab0275e3d53479e5cb358c474de875861e0
leo <40744320+leogithubid@users.noreply.github.com>
2021-09-06 07:46:37 +1000

    Step 2: Creating a Model
    Step 2: Creating a Model

--- 02-sap-ui5-data-binding/webapp/index.js
+++ 02-sap-ui5-data-binding/webapp/index.js
@@ -1,7 +1,15 @@
 sap.ui.require([
-    "sap/m/Text"
-],function(Text){
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+],function(Text,JSONModel){
     "use strict";
+    //create an object of JSON Model and write some text to it
+    var oModel = new JSONModel({
+        greetingText : "Welcome to UI5 Data Binding - with JSON Model"
+    });
+
+    //attach the model to the core for now, in prodution apps - we do it for views
+    sap.ui.getCore().setModel(oModel);
 
     //attach a function to init event
     sap.ui.getCore().attachInit(function(){

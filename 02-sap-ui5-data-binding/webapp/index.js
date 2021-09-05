0716373674f9a1743e51b4c75274ac27c6e49a8a
leo <40744320+leogithubid@users.noreply.github.com>
2021-09-06 08:35:24 +1000

    Step 5: One-Way Data Binding
    Step 5: One-Way Data Binding

--- 02-sap-ui5-data-binding/webapp/index.js
+++ 02-sap-ui5-data-binding/webapp/index.js
@@ -1,17 +1,23 @@
 sap.ui.require([
     "sap/m/Text",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/mvc/XMLView"
-],function(Text,JSONModel,XMLView){
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/model/BindingMode"
+],function(Text,JSONModel,XMLView,BindingMode){
     "use strict";
 
     //create an object of JSON Model and write some text to it
     var oModel = new JSONModel({
+        //added new properites to json MODEL 
+        //these properties will be bound in the XML View
         panelHeaderText : "Welcome to UI5 Data Binding - with JSON Model",
         firstName : "Liyon",
         lastName : "SV",
         enabled : true
     });
+
+    //make the binding oneway
+    oModel.setDefaultBindingMode(BindingMode.OneWay);
 
     //attach the model to the core for now, in prodution apps - we do it for views
     sap.ui.getCore().setModel(oModel);
@@ -19,7 +25,7 @@ sap.ui.require([
     //attach a function to init event
     sap.ui.getCore().attachInit(function(){
         new XMLView({
-            viewName : "sap.ui.demo.db.view.App"
+            viewName : "sap.ui.demo.db.view.App" //point to view folder->App.view.xml
         }).placeAt("content");
     });
 });

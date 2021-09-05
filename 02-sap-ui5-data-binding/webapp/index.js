fb40fdbf593dc89d436e318efc8983967baf9024
leo <40744320+leogithubid@users.noreply.github.com>
2021-09-06 08:29:26 +1000

    Step 4: Two-Way Data Binding
    Step 4: Two-Way Data Binding

--- 02-sap-ui5-data-binding/webapp/index.js
+++ 02-sap-ui5-data-binding/webapp/index.js
@@ -1,11 +1,16 @@
 sap.ui.require([
     "sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-],function(Text,JSONModel){
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+],function(Text,JSONModel,XMLView){
     "use strict";
+
     //create an object of JSON Model and write some text to it
     var oModel = new JSONModel({
-        greetingText : "Welcome to UI5 Data Binding - with JSON Model"
+        panelHeaderText : "Welcome to UI5 Data Binding - with JSON Model",
+        firstName : "Liyon",
+        lastName : "SV",
+        enabled : true
     });
 
     //attach the model to the core for now, in prodution apps - we do it for views
@@ -13,8 +18,8 @@ sap.ui.require([
 
     //attach a function to init event
     sap.ui.getCore().attachInit(function(){
-        new Text({
-            text : "{/greetingText}"
+        new XMLView({
+            viewName : "sap.ui.demo.db.view.App"
         }).placeAt("content");
     });
 });
